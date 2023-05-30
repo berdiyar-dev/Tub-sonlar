1c00407071075256c1dc22fa24f5967cf1377bbe
berdiyar-desktop <berdiyare@gmail.com>
2023-05-31 00:00:30 +0500

    user2

--- tub.py
+++ tub.py
@@ -1,8 +1,8 @@
-a = int(input("Son kiriting: "))
+a = int(input("Sonlar kiriting: "))
 b = 0
 for i in range(1,a+1):
     b += a % i == 0
 if b == 2 and a > 1:
-    print("Tub")
+    print("Tub son")
 else:
-    print("Bu tub sono emas !")
+    print("Bu tub son emas !")
